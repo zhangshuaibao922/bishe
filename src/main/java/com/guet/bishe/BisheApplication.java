7ba6f7f96124dedd9bcfabecd9648ac61aef12d5
zhangshuaibao <zhangshuaibao@bixin.cn>
2024-02-27 14:12:36 +0800

    初始化

---
+++ src/main/java/com/guet/bishe/BisheApplication.java
@@ -0,0 +1,13 @@
+package com.guet.bishe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BisheApplication.class, args);
+    }
+
+}
