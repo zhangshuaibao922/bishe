a92cef2fd3e1b6a4a37affa8739f3697f66d05c6
zhangshuaibao922 <2742520302@qq.com>
2024-04-24 14:56:28 +0800

    图片批注

--- src/main/java/com/guet/bishe/controller/OssImageUploadController.java
+++ src/main/java/com/guet/bishe/controller/OssImageUploadController.java
@@ -12,6 +12,8 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.websocket.server.PathParam;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -21,25 +23,17 @@ import java.io.InputStream;
 @RequestMapping("/aliyun")
 public class OssImageUploadController {
 
+    private static final String endpoint = "https://oss-cn-hongkong.aliyuncs.com";
+    private static final String accessKeyId = "LTAI5tNmSxaPFBVcCbVi5VSV";
+    private static final String accessKeySecret = "y0d3yr6Q0889ZjCvCascrMzYG6UMOO";
+    private static final String bucketName = "bishe-zhangshuaibao922";
     @Autowired
     PaperService paperService;
-
     @Autowired
     ExamMapper examMapper;
 
-    private static final String endpoint="https://oss-cn-hongkong.aliyuncs.com";
-
-
-    private static final String accessKeyId="LTAI5tNmSxaPFBVcCbVi5VSV";
-
-
-    private static final String accessKeySecret="y0d3yr6Q0889ZjCvCascrMzYG6UMOO";
-
-    private static final String bucketName="bishe-zhangshuaibao922";
-
-
     @PostMapping("/upload/{lessonId}/{examId}")
-    public String uploadImages(@PathVariable String lessonId,@PathVariable String examId,@RequestParam("images") MultipartFile[] images) {
+    public String uploadImages(@PathVariable String lessonId, @PathVariable String examId, @RequestParam("images") MultipartFile[] images) {
         // 创建 OSS 客户端
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         System.out.println("开始上传");
@@ -48,7 +42,7 @@ public class OssImageUploadController {
                 // 获取原始文件名
                 String originalFilename = image.getOriginalFilename();
                 String[] split = originalFilename.split("/");
-                String objectName=examId+"/resource/"+split[1];
+                String objectName = examId + "/resource/" + split[1];
                 System.out.println(split[1]);
                 // 上传文件流
                 InputStream inputStream = image.getInputStream();
@@ -64,8 +58,38 @@ public class OssImageUploadController {
 
                 Exam exam = examMapper.selectOne(new LambdaQueryWrapper<Exam>().eq(Exam::getExamId, examId));
 
-                paperService.insertByExamId(lessonId,examId,objectName);
+                paperService.insertByExamId(lessonId, examId, objectName);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "上传失败";
+        } finally {
+            // 关闭 OSS 客户端
+            ossClient.shutdown();
+        }
+
+
+        return "上传成功";
+    }
+
+    @PostMapping("/update")
+    public String uploadImage(@PathParam("objectName")String objectName, @RequestParam("image") MultipartFile image) {
+        // 创建 OSS 客户端
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        System.out.println("开始上传");
+        try {
+            objectName=objectName.split("top/")[1];
+            // 上传文件流
+            InputStream inputStream = image.getInputStream();
+
+            // 设置文件元信息
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(inputStream.available());
+
+            // 上传文件
+            PutObjectResult putObjectResult = ossClient.putObject(bucketName, objectName, inputStream);
+            System.out.println("成功上传文件：" + objectName);
+
         } catch (IOException e) {
             e.printStackTrace();
             return "上传失败";
