e879a716900a505604dd4ec8219070db321a1c3c
zhangshuaibao922 <2742520302@qq.com>
2024-05-09 16:49:32 +0800

    毕设done

---
+++ src/main/java/com/guet/bishe/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.guet.bishe.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration //配置类
+@EnableSwagger2// 开启Swagger2的自动配置
+public class SwaggerConfig {
+
+
+}
