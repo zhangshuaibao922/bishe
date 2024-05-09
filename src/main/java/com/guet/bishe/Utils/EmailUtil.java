96916807b8a57185be6a8f12f468bdf76f29ea0d
zhangshuaibao922 <2742520302@qq.com>
2024-05-09 22:12:46 +0800

    毕设done

--- src/main/java/com/guet/bishe/Utils/EmailUtil.java
+++ src/main/java/com/guet/bishe/Utils/EmailUtil.java
@@ -74,41 +74,41 @@ public class EmailUtil {
         });
     }
 
-    public void sendSuccess(String[] emails,String examName,String lessonName) {
-                    // 发送邮件
-                    JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-                    javaMailSender.setHost("smtp.qq.com");
-                    javaMailSender.setPort(465);
-                    javaMailSender.setUsername(user);
-                    javaMailSender.setPassword(code);
-                    javaMailSender.setDefaultEncoding("UTF-8");
+    public void sendSuccess(String[] emails, String examName, String lessonName) {
+        // 发送邮件
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.qq.com");
+        javaMailSender.setPort(465);
+        javaMailSender.setUsername(user);
+        javaMailSender.setPassword(code);
+        javaMailSender.setDefaultEncoding("UTF-8");
 
-                    Properties properties = new Properties();
-                    properties.setProperty("mail.smtp.timeout", "30000");
-                    properties.setProperty("mail.smtp.auth", "true");
-                    properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-                    javaMailSender.setJavaMailProperties(properties);
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.timeout", "30000");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        javaMailSender.setJavaMailProperties(properties);
 
-                    // 构建一个邮件对象
-                    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-                    // true表示构建一个可以带附件的邮件对象
-                    try {
-                        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-                        // 设置邮件主题
-                        mimeMessageHelper.setSubject("考试结束");
-                        // 设置邮件发送者
-                        mimeMessageHelper.setFrom(user);
-                        // 设置邮件接收者，可以有多个接收者
-                        mimeMessageHelper.setTo(emails);
-                        // 设置邮件发送日期
-                        mimeMessageHelper.setSentDate(new Date());
-                        // 设置邮件的正文
-                        String emailContent = lessonName + "的" + examName + "考试已经结束了，请去查询你的成绩。\n<a href=\"http://localhost:9999/\">点击这里</a>查看。";
-                        mimeMessageHelper.setText(emailContent,true);
-                        javaMailSender.send(mimeMessage);
-                    } catch (MessagingException e) {
-                        log.error("待审批邮件发送失败", e);
-                    }
-                }
+        // 构建一个邮件对象
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        // true表示构建一个可以带附件的邮件对象
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            // 设置邮件主题
+            mimeMessageHelper.setSubject("考试结束");
+            // 设置邮件发送者
+            mimeMessageHelper.setFrom(user);
+            // 设置邮件接收者，可以有多个接收者
+            mimeMessageHelper.setTo(emails);
+            // 设置邮件发送日期
+            mimeMessageHelper.setSentDate(new Date());
+            // 设置邮件的正文
+            String emailContent = lessonName + "的" + examName + "考试已经结束了，请去查询你的成绩。\n<a href=\"http://localhost:9999/\">点击这里</a>查看。";
+            mimeMessageHelper.setText(emailContent, true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            log.error("待审批邮件发送失败", e);
+        }
+    }
 
 }
